71c62759278d69f7776ec818f29529f1cdeaf6d4
onellvine <50703714+onellvine@users.noreply.github.com>
2019-11-10 19:37:42 +0300

    Add files via upload

---
+++ Analysis/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+data = pd.read_csv("C:\\Users\\Nel\\Pictures\\Python\\automate\\Analysis\\orders_final_with_dates.csv")
+
+price = list(data['Price'])
+total = price.pop()
+price.append(total)
+price.remove(total)
+
+
+days = list(data['Date'])
+last_day = days.pop()
+days.append(last_day)
+days.remove(last_day)
+
+plt.figure(figsize=[10,4])
+
+plt.bar(days, price, ec='red', width = 0.5, color='#0504aa',alpha=0.7)
+plt.grid(axis='y', alpha=0.75)
+plt.title("Earning per day in KES.")
+plt.xlabel("Days")
+plt.ylabel("Amount (in KES)")
+plt.show()
---
+++ readmail.py
@@ -0,0 +1,70 @@
+import imapclient
+import pyzmail
+from pprint import pprint
+from datetime import datetime
+from getpass import getpass
+
+username = 'nelouma@yahoo.com'
+#password = getpass("Email address password ({}): ".format(username))
+password = 'nelyaan7'
+
+imapObj = imapclient.IMAPClient('imap.mail.yahoo.com', ssl=True)
+
+imapObj.login(username, password)
+
+imapObj.select_folder('inbox', readonly=True)
+
+email_ids = imapObj.search(['SINCE', '02-Nov-2019', 'FROM', 'collinsmeroka@gmail.com', 'BEFORE', '11-Nov-2019'])
+
+with open('orders.txt', 'w') as f:
+    f.write("Serial no.,Date,Order no.,Order title,Page Count,Price\n")
+
+total = 0
+
+for i in range(len(email_ids)):
+    raw_messages = imapObj.fetch([email_ids[i]], ['BODY[]', 'INTERNALDATE', 'FLAGS'])
+
+    current_msg = raw_messages[email_ids[i]]
+    msg_date = current_msg['INTERNALDATE'.encode('utf-8')] # to get the date the email was sent
+    date_sent = datetime.strftime(msg_date, "%d %b %Y") # formating the date
+
+    message = pyzmail.PyzMessage.factory(raw_messages[email_ids[i]][b'BODY[]'])
+
+    subject = message.get_subject()
+
+    pprint(subject)
+    pprint(date_sent)
+    
+
+    if message.text_part != None:
+        #print("++++++++++++++++ Text Email +++++++++++++++++")
+        text_body = message.text_part.get_payload().decode(message.text_part.charset)
+        #print(text_body)
+        pages = text_body.partition("Pages:")[2] #get everything after the word "Pages:"
+        head, sep, tail = pages.partition(" pages") # take only the part before  pages/number of words
+        print("pages = {}".format(head.lstrip()))
+        #print("type of body", type(text_body))
+        
+    elif message.html_part != None:        
+        print("++++++++++++++++ HTML Email +++++++++++++++++")
+        html_body = message.html_part.get_payload().decode(message.html_part.charset)
+        print(html_body)
+        print("type of body", type(html_body))
+
+
+    with open('orders.txt', 'a') as f:
+        order_num = [int(s) for s in subject.split() if s.isdigit()] #extract order number from email subject
+        order_title = ''.join(char for char in subject if not char.isdigit()) #clean email subject by removing order number
+        pre, separator, suc = order_title.partition("\r")
+        if head != "":
+            price = int(head)*250
+        if len(order_num) != 0 and head != "":
+            f.write(str(i) +","+ date_sent +","+ str(order_num[0]) +","+ pre.replace(',','') +","+ head.lstrip() +","+str(price)+"\n")
+            total += price
+        else:
+            pass
+
+with open('orders.txt', 'a') as f:
+    f.write("Total,,,,,{}".format(total))
+
+    
